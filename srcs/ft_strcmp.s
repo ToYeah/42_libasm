5717f65c04cbf6873bb2ad1cb8be8df20e0d19ca
Taisei Toya <tytaisei0102@gmail.com>
2021-02-07 07:23:15 +0100

    FIx: strcmp

--- srcs/ft_strcmp.s
+++ srcs/ft_strcmp.s
@@ -24,12 +24,5 @@ ft_strcmp:
     mov al, byte [rdi + rcx]; dil = s1[rcx]
     xor rdi, rdi
     mov dil, byte [rsi + rcx]; sil = s2[rcx]
-    cmp al, dil
-    jb .end_smaller; if(dil < sil)
-.end_bigger:
-    sub rax, rdi
-    ret
-
-.end_smaller:
-    sub rax, rdi
+    sub eax, edi
     ret
